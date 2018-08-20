08d2e627aaff456016c3c53d52780994692fd5ea
zhangpei <zhangpe0312@qq.com>
2018-08-20 22:50:43 +0800

    多电脑、多设备、多任务执行完善中

--- src/main/java/org/nix/learn/auto/task/example/ExecutePhone.java
+++ src/main/java/org/nix/learn/auto/task/example/ExecutePhone.java
@@ -10,5 +10,4 @@ public class ExecutePhone {
     private String appiumPath;
 
 
-
 }
---
+++ src/main/java/org/nix/learn/auto/task/example/TaskList.java
@@ -0,0 +1,21 @@
+package org.nix.learn.auto.task.example;
+
+import java.util.List;
+
+/**
+ * 任务列表
+ * @author zhangpei
+ * @version 1.0
+ * @date 2018/8/20
+ */
+public class TaskList {
+
+    /**
+     * https://blog.csdn.net/qq_14908027/article/details/78886386 执行方案
+     * 将要执行任务的服务器列表
+     */
+    private List<AppiumServer> appiumServers;
+
+    private List<String> schemas;
+
+}
