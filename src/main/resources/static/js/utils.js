48a1917644130f9e27a616c3f19fa26e7a21c602
zhangpei <zhangpe0312@qq.com>
2018-08-26 14:13:01 +0800

    提交模板上线，下一步做交互设计

---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,11 @@
+function isValidIP(ip) {
+    var reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
+    return reg.test(ip);
+} 
+
+function isPort(port){
+	if(port>1023&&port<65535){
+		return true;
+	}
+	return false;
+}
