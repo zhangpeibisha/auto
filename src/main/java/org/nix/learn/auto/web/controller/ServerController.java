f04d4d199137c4d3e42ea9d8c9d7cb8f1b68b581
mac <zhangpei341@pingan.com.cn>
2018-08-27 16:03:23 +0800

    完成提交页面

--- src/main/java/org/nix/learn/auto/web/controller/ServerController.java
+++ src/main/java/org/nix/learn/auto/web/controller/ServerController.java
@@ -1,22 +1,29 @@
 package org.nix.learn.auto.web.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.log4j.Logger;
+import org.nix.learn.auto.utils.LogUtils;
+import org.nix.learn.auto.web.dto.SchemaSeverDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+
 
 /**
  * @author zhangpei341@pingan.cn.com 2018/8/27 下午1:32
  * @version 1.0
  */
 @RequestMapping(value = "servers")
+@RestController
 public class ServerController {
 
-    private static final Logger logger = Logger.getLogger(ServerController.class);
-
-    public Map<String,Object> schemaServer(){
-
-        return  null;
+    @PostMapping("schemaServer")
+    public String schemaServer(@RequestBody Map value){
+        LogUtils.printLog("返回信息",value);
+        return JSON.toJSONString(value);
     }
 
 }
--- src/main/java/org/nix/learn/auto/web/dto/SchemaSeverDto.java
+++ src/main/java/org/nix/learn/auto/web/dto/SchemaSeverDto.java
@@ -2,6 +2,9 @@ package org.nix.learn.auto.web.dto;
 
 import org.apache.log4j.Logger;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author zhangpei341@pingan.cn.com 2018/8/27 下午1:38
  * @version 1.0
@@ -11,5 +14,83 @@ public class SchemaSeverDto {
     private static final Logger logger = Logger.getLogger(SchemaSeverDto.class);
 
 
+    private List<Equipment> equipment;
+
+    private Set<String> schema;
+
+
+    public SchemaSeverDto() {
+    }
+
+
+    public List<Equipment> getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(List<Equipment> equipment) {
+        this.equipment = equipment;
+    }
+
+    public Set<String> getSchema() {
+        return schema;
+    }
+
+    public void setSchema(Set<String> schema) {
+        this.schema = schema;
+    }
+
+
+    @Override
+    public String toString() {
+        return "SchemaSeverDto{" +
+                "equipment=" + equipment +
+                ", schema=" + schema +
+                '}';
+    }
+
+    static class Equipment{
+        private String ip;
+        private String port;
+        private String udid;
+
+        public Equipment(String ip, String port, String udid) {
+            this.ip = ip;
+            this.port = port;
+            this.udid = udid;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getPort() {
+            return port;
+        }
+
+        public void setPort(String port) {
+            this.port = port;
+        }
+
+        public String getUdid() {
+            return udid;
+        }
+
+        public void setUdid(String udid) {
+            this.udid = udid;
+        }
+
+        @Override
+        public String toString() {
+            return "Equipment{" +
+                    "ip='" + ip + '\'' +
+                    ", port='" + port + '\'' +
+                    ", udid='" + udid + '\'' +
+                    '}';
+        }
+    }
 
 }
